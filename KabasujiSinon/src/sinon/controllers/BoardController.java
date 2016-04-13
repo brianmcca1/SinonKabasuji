37c75e4716114e5702e8f9fd5242d80aca468cb1
PDebs <psdebrine@wpi.edu>
2016-04-13 18:35:38 -0400

    Controller Constructors

--- KabasujiSinon/src/sinon/controllers/BoardController.java
+++ KabasujiSinon/src/sinon/controllers/BoardController.java
@@ -1,5 +1,18 @@
 package sinon.controllers;
 
+import sinon.main.Kabasuji;
+import sinon.views.BoardView;
+
 public class BoardController {
 
+	Kabasuji kabasuji;
+	BoardView view;
+
+	public BoardController(Kabasuji kaba, BoardView view){
+		this.kabasuji = kaba;
+		this.view = view;
+	}
+	
+	
+	
 }
