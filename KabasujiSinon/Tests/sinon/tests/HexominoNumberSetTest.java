b57ccfedfdca87cadbaf4dada9606f05a4b92de5
SkiingxMoose <jodesmond@live.com>
2016-04-21 16:28:45 -0400

    Changed many uses of set to list

--- KabasujiSinon/Tests/sinon/tests/HexominoNumberSetTest.java
+++ KabasujiSinon/Tests/sinon/tests/HexominoNumberSetTest.java
@@ -3,8 +3,8 @@ package sinon.tests;
 import static org.junit.Assert.assertEquals;
 
 import java.awt.Point;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -21,7 +21,7 @@ public class HexominoNumberSetTest {
 
     @Test
     public void testCreateLegalNumberSet() {
-        Set<Point> points = getLegalNumberSet();
+        List<Point> points = getLegalNumberSet();
 
         @SuppressWarnings("unused")
         HexominoNumberSet set = new HexominoNumberSet(points);
@@ -31,8 +31,8 @@ public class HexominoNumberSetTest {
      * Returns a straight line of six points. Is used to quickly get a legal
      * HexominoNumberSet
      */
-    private Set<Point> getLegalNumberSet() {
-        Set<Point> points = new HashSet<Point>();
+    private List<Point> getLegalNumberSet() {
+        List<Point> points = new LinkedList<Point>();
         for (int i = 0; i < 6; i++) {
             points.add(new Point(0, i));
         }
@@ -43,7 +43,7 @@ public class HexominoNumberSetTest {
     @Test(expected = IllegalArgumentException.class)
     public void testTooManyNumbers() {
         // Tests if you can create a HexominoNumberSet with too many numbers.
-        Set<Point> points = getLegalNumberSet();
+        List<Point> points = getLegalNumberSet();
         points.add(new Point(0, 6));
 
         @SuppressWarnings("unused")
@@ -56,9 +56,9 @@ public class HexominoNumberSetTest {
         HexominoNumberSet set = new HexominoNumberSet(-3, 0, -2, 0, -1, 0, 0, 0,
                 1, 0, 2, 0);
 
-        Set<Point> newPoints = set.getNormalizedPoints();
+        List<Point> newPoints = set.getNormalizedPoints();
 
-        Set<Point> points = new HashSet<Point>();
+        List<Point> points = new LinkedList<Point>();
         for (int i = 0; i < 6; i++) {
             points.add(new Point(i, 0));
         }
--- KabasujiSinon/src/sinon/models/Hexomino.java
+++ KabasujiSinon/src/sinon/models/Hexomino.java
@@ -2,6 +2,8 @@ package sinon.models;
 
 import java.awt.Point;
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -15,111 +17,116 @@ import java.util.Set;
  */
 public class Hexomino implements Serializable {
 
-	/** @see HexominoNumberSet */
-	HexominoNumberSet hexominoNumberSet;
+    /** @see HexominoNumberSet */
+    HexominoNumberSet hexominoNumberSet;
 
-	/**
-	 * Is a convenience constructor for quickly building a Hexomino.
-	 * 
-	 * @param a
-	 *            The x component of the first coordinate point
-	 * @param a1
-	 *            the y component of the first coordinate point
-	 */
-	public Hexomino(int a, int a1, int b, int b1, int c, int c1, int d, int d1, int e, int e1, int f, int f1) {
+    /**
+     * Is a convenience constructor for quickly building a Hexomino.
+     * 
+     * @param a
+     *            The x component of the first coordinate point
+     * @param a1
+     *            the y component of the first coordinate point
+     */
+    public Hexomino(int a, int a1, int b, int b1, int c, int c1, int d, int d1,
+            int e, int e1, int f, int f1) {
 
-		this.hexominoNumberSet = new HexominoNumberSet(a, a1, b, b1, c, c1, d, d1, e, e1, f, f1);
+        this.hexominoNumberSet = new HexominoNumberSet(a, a1, b, b1, c, c1, d,
+                d1, e, e1, f, f1);
 
-	}
+    }
 
-	public Hexomino(Set<Point> points) {
-		if (points == null) {
-			throw new IllegalArgumentException("Points can't be null");
-		}
-		this.hexominoNumberSet = new HexominoNumberSet(points);
-	}
+    public Hexomino(Set<Point> points) {
+        if (points == null) {
+            throw new IllegalArgumentException("Points can't be null");
+        }
 
-	/**
-	 * TODO
-	 * 
-	 * @param hexominoNumberSet
-	 */
-	public Hexomino(HexominoNumberSet hexominoNumberSet) {
-		this.hexominoNumberSet = hexominoNumberSet;
-	}
+        List<Point> listOfPoints = new LinkedList<Point>(points);
+        this.hexominoNumberSet = new HexominoNumberSet(listOfPoints);
+    }
 
-	protected void addToTile(Tile t) {
-		t.addHexomino(this);
-	}
+    /**
+     * TODO
+     * 
+     * @param hexominoNumberSet
+     */
+    public Hexomino(HexominoNumberSet hexominoNumberSet) {
+        this.hexominoNumberSet = hexominoNumberSet;
+    }
 
-	public void flipHorizontally() {
-		hexominoNumberSet.flipHorizontally();
-	}
+    public void addToTile(Tile t) {
+        t.addHexomino(this);
+    }
 
-	public void flipVertically() {
-		hexominoNumberSet.flipVertically();
-	}
+    public void flipHorizontally() {
+        hexominoNumberSet.flipHorizontally();
+    }
 
-	public void rotateC() {
-		hexominoNumberSet.rotateC();
-	}
+    public void flipVertically() {
+        hexominoNumberSet.flipVertically();
+    }
 
-	public void rotateCC() {
-		hexominoNumberSet.rotateCC();
-	}
+    public void rotateC() {
+        hexominoNumberSet.rotateC();
+    }
 
-	/**
-	 * Returns the translated set of points, such that every point is positive,
-	 * and within the bounds of a 6x6 grid.
-	 * 
-	 * @return Returns a set of points that describes the Hexomino
-	 */
-	public Set<Point> getNormalizedPoints() {
-		return this.hexominoNumberSet.getNormalizedPoints();
-	}
+    public void rotateCC() {
+        hexominoNumberSet.rotateCC();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Hexomino))
-			return false;
-		Hexomino other = (Hexomino) obj;
-		if (hexominoNumberSet == null) {
-			if (other.hexominoNumberSet != null)
-				return false;
-		} else if (!hexominoNumberSet.equals(other.hexominoNumberSet))
-			return false;
-		return true;
-	}
+    /**
+     * Returns the translated set of points, such that every point is positive,
+     * and within the bounds of a 6x6 grid.
+     * 
+     * @return Returns a set of points that describes the Hexomino
+     */
+    public List<Point> getNormalizedPoints() {
+        return this.hexominoNumberSet.getNormalizedPoints();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((hexominoNumberSet == null) ? 0 : hexominoNumberSet.hashCode());
-		return result;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Hexomino))
+            return false;
+        Hexomino other = (Hexomino) obj;
+        if (hexominoNumberSet == null) {
+            if (other.hexominoNumberSet != null)
+                return false;
+        } else if (!hexominoNumberSet.equals(other.hexominoNumberSet))
+            return false;
+        return true;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Hexomino [hexominoNumberSet=" + hexominoNumberSet + "]";
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((hexominoNumberSet == null) ? 0
+                : hexominoNumberSet.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Hexomino [hexominoNumberSet=" + hexominoNumberSet + "]";
+    }
 }
