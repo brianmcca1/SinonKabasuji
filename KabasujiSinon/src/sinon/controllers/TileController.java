fb610da8b55059ec680dd7f62d66c55cedb3e22d
SkiingxMoose <jodesmond@live.com>
2016-04-21 18:29:58 -0400

    Fixed up TileController

--- KabasujiSinon/src/sinon/controllers/TileController.java
+++ KabasujiSinon/src/sinon/controllers/TileController.java
@@ -1,38 +1,43 @@
 package sinon.controllers;
 
-import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-import sinon.main.Kabasuji;
 import sinon.models.Board;
 import sinon.models.Hexomino;
+import sinon.models.Level;
 import sinon.models.Tile;
 import sinon.views.TileView;
 
-public class TileController implements MouseListener {
+/**
+ * A TileController is the mouse listener for a TileView.
+ * 
+ * A TileController is registered to an individual tile and tileView. Each
+ * TileController must also be aware of it's current level, so that it can
+ * access the board and know the selected piece.
+ * 
+ * @author Peter Debrine
+ * @author Josh Desmond
+ */
+public class TileController implements MouseListener, MouseMotionListener {
 
-    Kabasuji kabasuji;
+    Level level;
     TileView view;
     Tile tile;
 
-    public TileController(Kabasuji kabasuji, TileView view, Point point) {
-        this.kabasuji = kabasuji;
+    public TileController(Level level, TileView view, Tile tile) {
+        this.level = level;
         this.view = view;
-        //this.tile = kabasuji.openedLevel.board.getTile(point.x, point.y);
+        this.tile = tile;
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("I AM A TILE AND I WAS JUST CLICKED ON!");
-        // The possible outcomes from this event are
-        // create a board to board
-        // create a bullpen to board
-        // select a hexomino
-        // or do nothing
 
-        if (kabasuji.hasSelected()) {
-            final Hexomino selectedHex = kabasuji.selected.get();
+        if (level.hasSelected()) {
+            final Hexomino selectedHex = level.selected.get();
 
             if (getBoard().canAddHexomino(tile.getLocation(), selectedHex)) {
 
@@ -44,39 +49,41 @@ public class TileController implements MouseListener {
         }
 
         else {
-            assert (!kabasuji.hasSelected());
+            assert (!level.hasSelected());
             if (tile.hasHex()) {
-                kabasuji.select(tile.getHexomino());
+                level.select(tile.getHexomino());
             }
         }
     }
 
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO handle shadows.
+    }
+
+    /** Accesses the board from the fields this class has */
     private Board getBoard() {
-        return kabasuji.openedLevel.board;
+        return level.board;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        // TODO shadows
-
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        // TODO shadows
+    }
 
+    @Override
+    public void mouseDragged(MouseEvent e) {
     }
 
 }
