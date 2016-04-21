9e60188daf8f8af8c71dceed5362ec21fe9028ec
SkiingxMoose <jodesmond@live.com>
2016-04-21 17:09:05 -0400

    Added the updated LevelData with contracts

--- KabasujiSinon/src/sinon/models/LevelData.java
+++ KabasujiSinon/src/sinon/models/LevelData.java
@@ -2,23 +2,65 @@ package sinon.models;
 
 import java.io.Serializable;
 
-/** This class holds all of the data necessary to save/load a level. */
-public class LevelData implements Serializable{
-	/** Board that the level will be played on.*/
-    Board board;
-    /** Bullpen holding all the level hexominoes.*/
-    BullPen bullpen;
-    /** This level's number (1 to 15).*/
-    int levelNum;
-    /** "puzzle", "lightning", or "release" level types.*/
-    String levelType;
+/**
+ * This class holds all of the data necessary to save/load a level.
+ */
+public class LevelData implements Serializable {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3147645287701832176L;
 
-    public Board getBoard(){ return this.board;}
-    
-    public BullPen getBullpen(){ return this.bullpen;}
-    
-    public int getLevelNum(){ return this.levelNum;}
- 
-    public String getLevelType(){ return this.levelType;}
-    
+    /** Enum representing three types of level */
+    enum LevelType {
+        PUZZLE, LIGHTNING, RELEASE
+    }
+
+    /** Type of level */
+    LevelType levelType;
+    /** Board that the level will be played on. */
+    BoardData board;
+    /** Bullpen holding all the level hexominoes. */
+    BullPenData bullpen;
+    /** This level's number (1 to 15). */
+    int levelNum;
+
+    /**
+     * Builds an empty board which is ready for game play based on the data
+     * stored in this object.
+     * 
+     * @return a Board for game play.
+     */
+    public Board getBoard() {
+        return null;
+    }
+
+    /**
+     * Builds a BullPen which is ready for game play based on the data stored in
+     * this object.
+     * 
+     * @return a Bullpen for game play.
+     */
+    public BullPen getBullpen() {
+        return null;
+    }
+
+    /**
+     * Gets the number of this level.
+     * 
+     * @return Value between 1 and 15
+     */
+    public int getLevelNum() {
+        return this.levelNum;
+    }
+
+    /**
+     * Gets the enum value of the type of level.
+     * 
+     * @return The type of level.
+     */
+    public LevelType getLevelType() {
+        return this.levelType;
+    }
+
 }
