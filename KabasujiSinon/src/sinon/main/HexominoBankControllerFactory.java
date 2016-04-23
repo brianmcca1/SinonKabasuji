3999e79e236b9e639c0c615c14ac37f7b7922227
SkiingxMoose <jodesmond@live.com>
2016-04-23 17:12:39 -0400

    Added a method in factory

--- KabasujiSinon/src/sinon/main/HexominoBankControllerFactory.java
+++ KabasujiSinon/src/sinon/main/HexominoBankControllerFactory.java
@@ -1,5 +1,13 @@
 package sinon.main;
 
+import sinon.models.BullPen;
+import sinon.views.HexominoBullpenView;
+
 public class HexominoBankControllerFactory {
+
+    public void registerHexomino(HexominoBullpenView view, BullPen bullpen) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
