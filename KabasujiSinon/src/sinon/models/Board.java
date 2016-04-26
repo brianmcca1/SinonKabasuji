b843e200cb4b2c290a02b5b0e0993d7a9252d633
SkiingxMoose <jodesmond@live.com>
2016-04-26 00:07:48 -0400

    merge resolution

--- KabasujiSinon/src/sinon/models/Board.java
+++ KabasujiSinon/src/sinon/models/Board.java
@@ -15,175 +15,198 @@ import sinon.models.data.BoardData;
  **/
 public class Board {
 
-    private final static int WIDTH = 12;
-    private final static int HEIGHT = 12;
-    private final static int WIDTH_BY_ZERO = WIDTH - 1;
-    private final static int HEIGHT_BY_ZERO = HEIGHT - 1;
+	private final static int WIDTH = 12;
+	private final static int HEIGHT = 12;
+	private final static int WIDTH_BY_ZERO = WIDTH - 1;
+	private final static int HEIGHT_BY_ZERO = HEIGHT - 1;
 
-    Map<Point, Tile> tilesViaPoints;
+	Map<Point, Tile> tilesViaPoints;
+	/** Maps every Hexomino to the tile of its anchor location. */
+	Map<Hexomino, Point> hexominoLocations;
 
-    /**
-     * Creates a 12 by 12 Board with tiles initialized to all playable tiles.
-     */
-    public Board() {
-        tilesViaPoints = new HashMap<Point, Tile>();
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                Point p = new Point(x, y);
-                tilesViaPoints.put(p, new Tile(p, true));
-            }
-        }
-    }
+	/**
+	 * Creates a 12 by 12 Board with tiles initialized to all playable tiles.
+	 */
+	public Board() {
+		hexominoLocations = new HashMap<Hexomino, Point>();
+		tilesViaPoints = new HashMap<Point, Tile>();
+		for (int x = 0; x < WIDTH; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+				Point p = new Point(x, y);
+				tilesViaPoints.put(p, new Tile(p, true));
+			}
+		}
+	}
 
-    /**
-     * Creates a 12 by 12 Board with playability set to match the given
-     * BoardData.
-     * 
-     * @param boardData
-     *            BoardData specifying which tiles are playable.
-     */
-    public Board(BoardData boardData) {
-    	tilesViaPoints = new HashMap<Point, Tile>();
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                boolean playable = boardData.getPlayableArray()[x][y];
-                Point p = new Point(x, y);
-                tilesViaPoints.put(p, new Tile(p, playable));
-            }
-        }
-    }
+	/**
+	 * Creates a 12 by 12 Board with playability set to match the given
+	 * BoardData.
+	 * 
+	 * @param boardData
+	 *            BoardData specifying which tiles are playable.
+	 */
+	public Board(BoardData boardData) {
+		hexominoLocations = new HashMap<Hexomino, Point>();
+		tilesViaPoints = new HashMap<Point, Tile>();
+		for (int x = 0; x < WIDTH; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+				boolean playable = boardData.getPlayableArray()[x][y];
+				Point p = new Point(x, y);
+				tilesViaPoints.put(p, new Tile(p, playable));
+			}
+		}
+	}
 
-    /**
-     * Determines if there is a hexomino at the given position.
-     * 
-     * @param row
-     * @param column
-     * @return True if there is a hexomino on this tile
-     */
-    public boolean hasHex(int row, int column) {
-        return getTile(row, column).hasHex();
-    }
+	/**
+	 * Determines if there is a hexomino at the given position.
+	 * 
+	 * @param row
+	 * @param column
+	 * @return True if there is a hexomino on this tile
+	 */
+	public boolean hasHex(int row, int column) {
+		return getTile(row, column).hasHex();
+	}
 
-    /**
-     * Determines whether it's possible to add a Hexomino at the given location.
-     * 
-     * @param anchorLocation
-     *            Location of where the anchor of the hexomino will go.
-     * @param hex
-     *            The Hexomino to be added
-     * @return True if the move if possible, false otherwise.
-     */
-    public boolean canAddHexomino(Point anchorLocation, Hexomino hex) {
-        boolean canAdd = true;
-        HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
-        for (Point p : hexominoNumberSet.getPoints()) {
-            Point pointToCheck = new Point(anchorLocation.x + p.x,
-                    anchorLocation.y + p.y);
-            if (!(this.isInBounds(pointToCheck.x, pointToCheck.y))){
-            	return false;
-            }
-            if (!tilesViaPoints.get(pointToCheck).canAddHex()) {
-                canAdd = false;
-            }
-        }
+	/**
+	 * Determines whether it's possible to add a Hexomino at the given location.
+	 * 
+	 * @param anchorLocation
+	 *            Location of where the anchor of the hexomino will go.
+	 * @param hex
+	 *            The Hexomino to be added
+	 * @return True if the move if possible, false otherwise.
+	 */
+	public boolean canAddHexomino(Point anchorLocation, Hexomino hex) {
+		boolean canAdd = true;
+		HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
+		for (Point p : hexominoNumberSet.getPoints()) {
+			Point pointToCheck = new Point(anchorLocation.x + p.x,
+					anchorLocation.y + p.y);
+			if (!(this.isInBounds(pointToCheck.x, pointToCheck.y))) {
+				return false;
+			}
+			if (!tilesViaPoints.get(pointToCheck).canAddHex()) {
+				canAdd = false;
+			}
+		}
 
-        return canAdd;
-    }
+		return canAdd;
+	}
 
-    /**
-     * Adds the given Hexomino to the board. Must be a valid move.
-     * 
-     * @param anchorLocation
-     *            Location of where the anchor of the hexomino will go.
-     * @param hex
-     *            The Hexomino to be added
-     * 
-     */
-    public void addHexomino(Point anchorLocation, Hexomino hex)
-             {
-       
+	/**
+	 * Retrieves the position where a hexomino is placed.
+	 * 
+	 * @param hex
+	 *            Hexomino to be found.
+	 * @return The Point p where the anchor of tile is located.
+	 */
+	public Point getHexominoLocation(Hexomino hex) {
+		return hexominoLocations.get(hex);
+	}
 
-        HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
-        for (Point p : hexominoNumberSet.getPoints()) {
-            Point pointToAdd = new Point(anchorLocation.x + p.x,
-                    anchorLocation.y + p.y);
-            if (tilesViaPoints.get(pointToAdd).canAddHex()
-                    & isInBounds(pointToAdd.x, pointToAdd.y)) {
-                tilesViaPoints.get(pointToAdd).addHexomino(hex);
-            }
-        }
+	/**
+	 * Adds the given Hexomino to the board. Must be a valid move.
+	 * 
+	 * @param anchorLocation
+	 *            Location of where the anchor of the hexomino will go.
+	 * @param hex
+	 *            The Hexomino to be added
+	 * 
+	 */
+	public void addHexomino(Point anchorLocation, Hexomino hex) {
+		HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
 
-    }
+		hexominoLocations.put(hex, anchorLocation.getLocation());
 
-    /**
-     * Gets the Tile at the specified position
-     * 
-     * @param row
-     * @param column
-     * @return The Tile found.
-     */
-    public Tile getTile(int row, int column) {
-        return tilesViaPoints.get(new Point(row, column));
-    }
+		for (Point p : hexominoNumberSet.getPoints()) {
+			Point pointToAdd = new Point(anchorLocation.x + p.x,
+					anchorLocation.y + p.y);
+			if (tilesViaPoints.get(pointToAdd).canAddHex()
+					& isInBounds(pointToAdd.x, pointToAdd.y)) {
+				tilesViaPoints.get(pointToAdd).addHexomino(hex);
+			}
+		}
+	}
 
-    public Tile getTile(Point p) {
-        return tilesViaPoints.get(p);
-    }
+	/**
+	 * Completely removes the specified Hexomino from the board.
+	 * 
+	 * Note that this removes the Hexomino only if it is the exact same
+	 * instance. If the Hexomino given is .equals() but not ==, it doesn't
+	 * matter, it won't be removed.
+	 */
+	public void removeHexomino(Hexomino hex) {
+		for (Tile t : getTiles()) {
+			if (t.getHexomino().isPresent()) {
+				if (t.getHexomino().get() == hex) { // TODO is this correct
+					t.removeHex();
+				}
+			}
+		}
 
-    /**
-     * Returns whether given row and column are in bounds in the existing game
-     * region. I.E. if the given row and column are outside of the 12 by 12
-     * grid, this will return false.
-     * 
-     * Note this has nothing to do with whether a tile is playable or not.
-     * 
-     * @param row
-     * @param column
-     * @return True if the location is in bounds
-     */
-    private boolean isInBounds(int row, int column) {
-        if (row > HEIGHT_BY_ZERO || row > WIDTH_BY_ZERO) {
-            return false;
-        } else if (row < 0 || column < 0) {
-            return false;
-        } else
-            return true;
-    }
+		hexominoLocations.remove(hex);
+	}
 
-    /**
-     * Completely removes the specified Hexomino from the board.
-     * 
-     * Note that this removes the Hexomino only if it is the exact same
-     * instance. If the Hexomino given is .equals() but not ==, it doesn't
-     * matter, it won't be removed.
-     */
-    public void removeHexomino(Hexomino hex) {
-        for (Tile t : getTiles()) {
-            if (t.getHexomino().isPresent()) {
-                if (t.getHexomino().get() == hex) { // TODO is this correct
-                    t.removeHex();
-                }
-            }
-        }
-    }
+	/**
+	 * Gets the Tile at the specified position
+	 * 
+	 * @param row
+	 * @param column
+	 * @return The Tile found.
+	 */
+	public Tile getTile(int row, int column) {
+		return getTile(new Point(row, column));
+	}
 
-    /**
-     * Gets a iterable of all the tiles.
-     * 
-     * You can use this any time you want to do some kind of operation on all of
-     * the tiles. Please be careful and don't actually edit the tiles with a
-     * controller or something unless it's really needed.
-     * 
-     * Note that these tiles are not in order if you use this method.
-     * 
-     * @return An iterable of all of the tiles.
-     */
-    public Iterable<Tile> getTiles() {
-        LinkedList<Tile> tempList = new LinkedList<Tile>();
-        tilesViaPoints.forEach((k, v) -> tempList.add(v));
-        return tempList;
-    }
+	/**
+	 * Gets the Tile at the specified position
+	 * 
+	 * @param p
+	 *            Point to get tile at.
+	 * @return The Tile found.
+	 */
+	public Tile getTile(Point p) {
+		return tilesViaPoints.get(p);
+	}
 
-    // TODO equals and hash and ToString
+	/**
+	 * Returns whether given row and column are in bounds in the existing game
+	 * region. I.E. if the given row and column are outside of the 12 by 12
+	 * grid, this will return false.
+	 * 
+	 * Note this has nothing to do with whether a tile is playable or not.
+	 * 
+	 * @param row
+	 * @param column
+	 * @return True if the location is in bounds
+	 */
+	private boolean isInBounds(int row, int column) {
+		if (row > HEIGHT_BY_ZERO || row > WIDTH_BY_ZERO) {
+			return false;
+		} else if (row < 0 || column < 0) {
+			return false;
+		} else
+			return true;
+	}
+
+	/**
+	 * Gets a iterable of all the tiles.
+	 * 
+	 * You can use this any time you want to do some kind of operation on all of
+	 * the tiles. Please be careful and don't actually edit the tiles with a
+	 * controller or something unless it's really needed.
+	 * 
+	 * Note that these tiles are not in order if you use this method.
+	 * 
+	 * @return An iterable of all of the tiles.
+	 */
+	public Iterable<Tile> getTiles() {
+		LinkedList<Tile> tempList = new LinkedList<Tile>();
+		tilesViaPoints.forEach((k, v) -> tempList.add(v));
+		return tempList;
+	}
+
+	// TODO equals and hash and ToString
 
 }
