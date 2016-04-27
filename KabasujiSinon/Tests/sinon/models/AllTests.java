5b819b72925246824d15834da81ce11b375fc10e
SkiingxMoose <jodesmond@live.com>
2016-04-26 20:57:41 -0400

    Included integration test in test suite

--- KabasujiSinon/Tests/sinon/models/AllTests.java
+++ KabasujiSinon/Tests/sinon/models/AllTests.java
@@ -4,12 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import sinon.models.HexominoTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({ HexominoNumberSetTest.class, HexominoTest.class,
-        NumberSetFactoryTest.class, TestBoard.class, TestBullpen.class,
-        TestLevel.class, TestLightingLevel.class, TestTile.class })
+        NumberSetFactoryTest.class, PuzzleIntegrationTest.class,
+        TestBoard.class, TestBullpen.class, TestLevel.class,
+        TestLightingLevel.class, TestTile.class })
 
 public class AllTests {
 
