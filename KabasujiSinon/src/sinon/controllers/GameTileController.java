95820284549237153e1e3ce537378d9052700f55
kartikvasu <kartiktv97@gmail.com>
2016-04-28 18:27:10 -0400

    More changes to tilecontroller. Mostly cleanup.

--- KabasujiSinon/src/sinon/controllers/GameTileController.java
+++ KabasujiSinon/src/sinon/controllers/GameTileController.java
@@ -24,51 +24,6 @@ public class GameTileController extends TileController {
 		return level.getBoard();
 	}
 
-	@Override
-	public void handleLeftClick() {
-		System.out.println("I AM A TILE AND I WAS JUST CLICKED ON!");
-
-		if (level.hasSelected()) {
-
-			if (level.getBullpen().containsHexID(level.getSelectedHexomino().get().getID())) {
-				// else move from bullpen to board
-				MoveToBoardFromBullpen move = new MoveToBoardFromBullpen(
-						this.level, this.tileView.getRow(),
-						this.tileView.getColumn());
-
-				if (move.doMove()) {
-					System.out.println("Move successfully completed!");
-					level.deselect();
-				} else {
-					System.out.println("There was some error doing the move.");
-				}
-			}
-
-			else if (level.getBoard().containsHexID(level.getSelectedHexomino().get().getID())) {
-				// then move from board to board
-				int x, y;
-				x = level.getBoard().getHexominoLocation(
-						level.getSelectedHexomino().get()).x;
-				y = level.getBoard().getHexominoLocation(
-						level.getSelectedHexomino().get()).y;
-				MoveInBoard move = new MoveInBoard(level, x, y,
-						this.tileView.getRow(), this.tileView.getColumn());
-
-				if (move.doMove()) {
-					System.out.println("The move was successfully completed!");
-					level.deselect();
-				} else {
-					System.out.println("The move couldn't be completed!");
-				}
-
-			}
-		} else {
-			if (this.tileView.getTile().hasHex()) {
-				level.select(this.tileView.getTile().getHexomino().get());
-			}
-		}
-
-	}
 
 	@Override
 	public void handleRightClick() {
--- KabasujiSinon/src/sinon/controllers/TileController.java
+++ KabasujiSinon/src/sinon/controllers/TileController.java
@@ -7,6 +7,8 @@ import javax.swing.SwingUtilities;
 
 import sinon.models.Level;
 import sinon.models.Tile;
+import sinon.moves.MoveInBoard;
+import sinon.moves.MoveToBoardFromBullpen;
 import sinon.views.MainView;
 import sinon.views.TileView;
 
@@ -62,5 +64,47 @@ public abstract class TileController implements MouseListener {
 
 	public abstract void handleRightClick();
 
-	public abstract void handleLeftClick();
+	public void handleLeftClick() {
+		System.out.println("I AM A TILE AND I WAS JUST CLICKED ON!");
+
+		if (level.hasSelected()) {
+
+			if (level.getBullpen().containsHexID(level.getSelectedHexomino().get().getID())) {
+				// else move from bullpen to board
+				MoveToBoardFromBullpen move = new MoveToBoardFromBullpen(
+						this.level, this.tileView.getRow(),
+						this.tileView.getColumn());
+
+				if (move.doMove()) {
+					System.out.println("Move successfully completed!");
+					level.deselect();
+				} else {
+					System.out.println("There was some error doing the move.");
+				}
+			}
+
+			else if (level.getBoard().containsHexID(level.getSelectedHexomino().get().getID())) {
+				// then move from board to board
+				int x, y;
+				x = level.getBoard().getHexominoLocation(
+						level.getSelectedHexomino().get()).x;
+				y = level.getBoard().getHexominoLocation(
+						level.getSelectedHexomino().get()).y;
+				MoveInBoard move = new MoveInBoard(level, x, y,
+						this.tileView.getRow(), this.tileView.getColumn());
+
+				if (move.doMove()) {
+					System.out.println("The move was successfully completed!");
+					level.deselect();
+				} else {
+					System.out.println("The move couldn't be completed!");
+				}
+
+			}
+		} else {
+			if (this.tileView.getTile().hasHex()) {
+				level.select(this.tileView.getTile().getHexomino().get());
+			}
+		}
+	}
 }
