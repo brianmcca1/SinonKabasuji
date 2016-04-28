ea181cc5f9895ffb92630abf803159450939d5d1
SkiingxMoose <jodesmond@live.com>
2016-04-28 13:29:30 -0400

    Removed some legacy classes that weren't really meant to be there.

--- KabasujiSinon/src/sinon/views/builder/BankView.java
+++ KabasujiSinon/src/sinon/views/builder/BankView.java
@@ -12,7 +12,6 @@ import javax.swing.JScrollPane;
 
 import sinon.controllers.HexominoBankController;
 import sinon.main.Builder;
-import sinon.main.HexominoBankControllerFactory;
 import sinon.models.BullPen;
 import sinon.views.HexominoBullpenView;
 import sinon.views.InfoPanel;
@@ -20,53 +19,53 @@ import sinon.views.InfoPanel;
 @SuppressWarnings("serial")
 public class BankView extends InfoPanel {
 
-    JScrollPane scrollPanel;
-    JPanel contentPanel;
-    BullPen bullpen;
-    Builder builder;
-    List<HexominoBullpenView> hexominoViews;
+	JScrollPane scrollPanel;
+	JPanel contentPanel;
+	BullPen bullpen;
+	Builder builder;
+	List<HexominoBullpenView> hexominoViews;
 
-    public BankView(Builder b, BullPen bullpen) {
-        super();
-        
-        this.builder = b;
-        this.bullpen = bullpen;
-        this.hexominoViews = new LinkedList<HexominoBullpenView>();
+	public BankView(Builder b, BullPen bullpen) {
+		super();
 
-        initContentPanel();
-        initBankViewScrollPanel();
-        populateBankViewWithHexominoes();
+		this.builder = b;
+		this.bullpen = bullpen;
+		this.hexominoViews = new LinkedList<HexominoBullpenView>();
 
-        this.setLayout(new GridLayout(1, 1));
-        this.add(scrollPanel);
-        this.validate();
-    }
+		initContentPanel();
+		initBankViewScrollPanel();
+		populateBankViewWithHexominoes();
 
-    private void initContentPanel() {
-        contentPanel = new JPanel();
-        contentPanel.setBackground(Color.orange);
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-    }
+		this.setLayout(new GridLayout(1, 1));
+		this.add(scrollPanel);
+		this.validate();
+	}
 
-    private void initBankViewScrollPanel() {
-        scrollPanel = new JScrollPane();
-        JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
-        scrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPanel.add(scrollBar);
-        scrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPanel.setViewportView(contentPanel);
-    }
+	private void initContentPanel() {
+		contentPanel = new JPanel();
+		contentPanel.setBackground(Color.orange);
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+	}
 
-    private void populateBankViewWithHexominoes() {
-        for (int i = 0; i < bullpen.getPieces().size(); i++) {
-            HexominoBullpenView tempHexBullpenView = new HexominoBullpenView(bullpen.getPieces().get(i));
-            this.contentPanel.add(tempHexBullpenView);
-            this.hexominoViews.add(tempHexBullpenView);
-            tempHexBullpenView.addMouseListener(new HexominoBankController(this.builder, this.builder.getLevel().getBullpen(), tempHexBullpenView));
-        }
+	private void initBankViewScrollPanel() {
+		scrollPanel = new JScrollPane();
+		JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
+		scrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPanel.add(scrollBar);
+		scrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPanel.setViewportView(contentPanel);
+	}
 
-        this.contentPanel.doLayout();
-        this.validate();
-    }
+	private void populateBankViewWithHexominoes() {
+		for (int i = 0; i < bullpen.getPieces().size(); i++) {
+			HexominoBullpenView tempHexBullpenView = new HexominoBullpenView(bullpen.getPieces().get(i));
+			this.contentPanel.add(tempHexBullpenView);
+			this.hexominoViews.add(tempHexBullpenView);
+			tempHexBullpenView.addMouseListener(new HexominoBankController(this.builder, this.builder.getLevel().getBullpen(), tempHexBullpenView));
+		}
+
+		this.contentPanel.doLayout();
+		this.validate();
+	}
 
 }
