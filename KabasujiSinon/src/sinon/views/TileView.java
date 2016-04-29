41c6369ab2871d218805692995c0dbb97f9d4ad9
SkiingxMoose <jodesmond@live.com>
2016-04-28 20:47:27 -0400

    Colors I think

--- KabasujiSinon/src/sinon/views/TileView.java
+++ KabasujiSinon/src/sinon/views/TileView.java
@@ -16,102 +16,102 @@ import sinon.models.Tile;
 @SuppressWarnings("serial")
 public class TileView extends JPanel implements Observer {
 
-	public Tile tile;
-	int row, column;
-	Color color;
+    public Tile tile;
+    int row, column;
+    Color color;
 
-	TileView(Tile tile) {
-		this.tile = tile;
-		this.setBackground(Color.white);
-		this.row = tile.getLocation().x;
-		this.column = tile.getLocation().y;
-		this.tile.registerObserver(this);
-	}
+    TileView(Tile tile) {
+        this.tile = tile;
+        this.setBackground(Color.white);
+        this.row = tile.getLocation().x;
+        this.column = tile.getLocation().y;
+        this.tile.registerObserver(this);
+    }
 
-	/**
-	 * Toggle the color from black to white, for unplayable and playable
-	 * respectively
-	 */
-	@Deprecated
-	public void toggleColor() {
-		if (!(this.getBackground().equals(Color.black)))
-			this.setBackground(Color.black);
-		else
-			this.setBackground(Color.white);
+    /**
+     * Toggle the color from black to white, for unplayable and playable
+     * respectively
+     */
+    @Deprecated
+    public void toggleColor() {
+        if (!(this.getBackground().equals(Color.black)))
+            this.setBackground(Color.black);
+        else
+            this.setBackground(Color.white);
 
-		this.repaint();
-		this.revalidate();
-	}
+        this.repaint();
+        this.revalidate();
+    }
 
-	/**
-	 * Sets this tiles color to the proper color based on the tile it's viewing,
-	 * and repaints the panel.
-	 */
-	protected void redraw() {
-		assert this.tile != null;
+    /**
+     * Sets this tiles color to the proper color based on the tile it's viewing,
+     * and repaints the panel.
+     */
+    protected void redraw() {
+        assert this.tile != null;
 
-		if (!tile.isPlayable()) {
-			this.setColor(Color.BLACK);
-		} else {
-			this.setColor(Color.WHITE);
-		}
+        if (!tile.isPlayable()) {
+            this.setColor(Color.BLACK);
+        } else {
+            this.setColor(Color.WHITE);
+        }
 
-		if (tile.hasHex()) {
-			// TODO set this to be tile.getHex().getColor();
-			// or to be tile.getColor();
-			this.setColor(Color.BLUE);
-		}
+        if (tile.hasHex()) {
+            // TODO set this to be tile.getHex().getColor();
+            // or to be tile.getColor();
+            this.setColor(tile.getHexomino().get().getColor());
+        }
 
-		else if (tile instanceof LightningTile) {
-			if (((LightningTile) tile).isLightninged()) {
-				this.setColor(Color.CYAN);
-			}
-		}
+        else if (tile instanceof LightningTile) {
+            if (((LightningTile) tile).isLightninged()) {
+                this.setColor(Color.CYAN);
+            }
+        }
 
-		this.repaint();
-		this.revalidate();
-	}
+        this.repaint();
+        this.revalidate();
+    }
 
-	/**
-	 * Get the row this tile is on
-	 * 
-	 * @return The row number
-	 */
-	public int getRow() {
-		return this.row;
-	}
+    /**
+     * Get the row this tile is on
+     * 
+     * @return The row number
+     */
+    public int getRow() {
+        return this.row;
+    }
 
-	/**
-	 * Get the column this tile is on
-	 * 
-	 * @return The column number
-	 */
-	public int getColumn() {
-		return this.column;
-	}
+    /**
+     * Get the column this tile is on
+     * 
+     * @return The column number
+     */
+    public int getColumn() {
+        return this.column;
+    }
 
-	/**
-	 * Get the tile this view is associated with
-	 * 
-	 * @return The tile the view is associated with
-	 */
-	public Tile getTile() {
-		return this.tile;
-	}
+    /**
+     * Get the tile this view is associated with
+     * 
+     * @return The tile the view is associated with
+     */
+    public Tile getTile() {
+        return this.tile;
+    }
 
-	/**
-	 * Set the color of the tile
-	 * 
-	 * @param c
-	 *            The color to set
-	 */
-	public void setColor(Color c) {
-		this.setBackground(c);
-	}
+    /**
+     * Set the color of the tile
+     * 
+     * @param c
+     *            The color to set
+     */
+    public void setColor(Color c) {
+        this.setBackground(c);
+    }
 
-	@Override
-	public void updated() {
-		this.redraw();
-	}
+    @Override
+    public void updated() {
+        this.redraw();
+    }
 
 }
