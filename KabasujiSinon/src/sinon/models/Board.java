4374ef91cbee81fd4651659a4224bceebe0681b4
SkiingxMoose <jodesmond@live.com>
2016-05-01 19:21:01 -0400

    Got hints to render once the level starts.

--- KabasujiSinon/src/sinon/models/Board.java
+++ KabasujiSinon/src/sinon/models/Board.java
@@ -18,345 +18,353 @@ import sinon.views.Observer;
  **/
 public class Board implements Observable {
 
-	/** The list of observers which are watching for updates on the board */
-	List<Observer> observers;
+    /** The list of observers which are watching for updates on the board */
+    List<Observer> observers;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Board [tilesViaPoints=" + tilesViaPoints + ", hexominoLocations=" + hexominoLocations + "]";
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Board [tilesViaPoints=" + tilesViaPoints
+                + ", hexominoLocations=" + hexominoLocations + "]";
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((hexominoLocations == null) ? 0 : hexominoLocations.hashCode());
-		result = prime * result + ((tilesViaPoints == null) ? 0 : tilesViaPoints.hashCode());
-		return result;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((hexominoLocations == null) ? 0
+                : hexominoLocations.hashCode());
+        result = prime * result
+                + ((tilesViaPoints == null) ? 0 : tilesViaPoints.hashCode());
+        return result;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Board other = (Board) obj;
-		if (hexominoLocations == null) {
-			if (other.hexominoLocations != null)
-				return false;
-		} else if (!hexominoLocations.equals(other.hexominoLocations))
-			return false;
-		if (tilesViaPoints == null) {
-			if (other.tilesViaPoints != null)
-				return false;
-		} else if (!tilesViaPoints.equals(other.tilesViaPoints))
-			return false;
-		return true;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Board other = (Board) obj;
+        if (hexominoLocations == null) {
+            if (other.hexominoLocations != null)
+                return false;
+        } else if (!hexominoLocations.equals(other.hexominoLocations))
+            return false;
+        if (tilesViaPoints == null) {
+            if (other.tilesViaPoints != null)
+                return false;
+        } else if (!tilesViaPoints.equals(other.tilesViaPoints))
+            return false;
+        return true;
+    }
 
-	private final static int WIDTH = 12;
-	private final static int HEIGHT = 12;
-	private final static int WIDTH_BY_ZERO = WIDTH - 1;
-	private final static int HEIGHT_BY_ZERO = HEIGHT - 1;
+    private final static int WIDTH = 12;
+    private final static int HEIGHT = 12;
+    private final static int WIDTH_BY_ZERO = WIDTH - 1;
+    private final static int HEIGHT_BY_ZERO = HEIGHT - 1;
 
-	Map<Point, Tile> tilesViaPoints;
-	/** Maps every Hexomino to the tile of its anchor location. */
-	Map<Hexomino, Point> hexominoLocations;
+    Map<Point, Tile> tilesViaPoints;
+    /** Maps every Hexomino to the tile of its anchor location. */
+    Map<Hexomino, Point> hexominoLocations;
 
-	List<Hint> hints;
-	/**
-	 * Creates a 12 by 12 Board with tiles initialized to all playable tiles.
-	 */
-	public Board() {
-		hexominoLocations = new HashMap<Hexomino, Point>();
-		tilesViaPoints = new HashMap<Point, Tile>();
-		for (int x = 0; x < WIDTH; x++) {
-			for (int y = 0; y < HEIGHT; y++) {
-				Point p = new Point(x, y);
-				initializeTiles(p, true);
-			}
-		}
-		this.hints = new LinkedList<Hint> ();
-		initializeObserverList();
-	}
+    List<Hint> hints;
 
-	/**
-	 * Creates a 12 by 12 Board with playability set to match the given
-	 * BoardData.
-	 * 
-	 * @param boardData
-	 *            BoardData specifying which tiles are playable.
-	 */
-	public Board(BoardData boardData) {
-		hexominoLocations = new HashMap<Hexomino, Point>();
-		tilesViaPoints = new HashMap<Point, Tile>();
-		for (int x = 0; x < WIDTH; x++) {
-			for (int y = 0; y < HEIGHT; y++) {
-				boolean playable = boardData.getPlayableArray()[x][y];
-				Point p = new Point(x, y);
-				initializeTiles(p, playable);
-			}
-		}
-		initializeObserverList();
-		this.hints = boardData.getHints();
-		update();
-	}
+    /**
+     * Creates a 12 by 12 Board with tiles initialized to all playable tiles.
+     */
+    public Board() {
+        hexominoLocations = new HashMap<Hexomino, Point>();
+        tilesViaPoints = new HashMap<Point, Tile>();
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                Point p = new Point(x, y);
+                initializeTiles(p, true);
+            }
+        }
+        this.hints = new LinkedList<Hint>();
+        initializeObserverList();
+    }
 
-	protected void initializeTiles(Point p, boolean playable) {
-		tilesViaPoints.put(p, new Tile(p, playable));
-	}
+    /**
+     * Creates a 12 by 12 Board with playability set to match the given
+     * BoardData.
+     * 
+     * @param boardData
+     *            BoardData specifying which tiles are playable.
+     */
+    public Board(BoardData boardData) {
+        hexominoLocations = new HashMap<Hexomino, Point>();
+        tilesViaPoints = new HashMap<Point, Tile>();
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                boolean playable = boardData.getPlayableArray()[x][y];
+                Point p = new Point(x, y);
+                initializeTiles(p, playable);
+            }
+        }
+        initializeObserverList();
+        this.hints = boardData.getHints();
+        update();
+    }
 
-	/** Every constructor should call this */
-	private void initializeObserverList() {
-		assert observers == null; // Why would you call this again?
-		observers = new LinkedList<Observer>();
-	}
+    protected void initializeTiles(Point p, boolean playable) {
+        tilesViaPoints.put(p, new Tile(p, playable));
+    }
 
-	/**
-	 * Determines if there is a hexomino at the given position.
-	 * 
-	 * @param row
-	 * @param column
-	 * @return True if there is a hexomino on this tile
-	 */
-	public boolean hasHex(int row, int column) {
-		return getTile(row, column).hasHex();
-	}
+    /** Every constructor should call this */
+    private void initializeObserverList() {
+        assert observers == null; // Why would you call this again?
+        observers = new LinkedList<Observer>();
+    }
 
-	/**
-	 * Determines whether it's possible to add a Hexomino at the given location.
-	 * 
-	 * @param anchorLocation
-	 *            Location of where the anchor of the hexomino will go.
-	 * @param hex
-	 *            The Hexomino to be added
-	 * @return True if the move if possible, false otherwise.
-	 */
-	public boolean canAddHexomino(Point anchorLocation, Hexomino hex) {
-		boolean canAdd = true;
-		HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
-		for (Point p : hexominoNumberSet.getPoints()) {
-			Point pointToCheck = new Point(anchorLocation.x + p.x, anchorLocation.y + p.y);
-			if (!(this.isInBounds(pointToCheck.x, pointToCheck.y))) {
-				return false;
-			}
-			if (!tilesViaPoints.get(pointToCheck).canAddHex(hex)) {
-				canAdd = false;
-			}
-		}
+    /**
+     * Determines if there is a hexomino at the given position.
+     * 
+     * @param row
+     * @param column
+     * @return True if there is a hexomino on this tile
+     */
+    public boolean hasHex(int row, int column) {
+        return getTile(row, column).hasHex();
+    }
 
-		return canAdd;
-	}
+    /**
+     * Determines whether it's possible to add a Hexomino at the given location.
+     * 
+     * @param anchorLocation
+     *            Location of where the anchor of the hexomino will go.
+     * @param hex
+     *            The Hexomino to be added
+     * @return True if the move if possible, false otherwise.
+     */
+    public boolean canAddHexomino(Point anchorLocation, Hexomino hex) {
+        boolean canAdd = true;
+        HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
+        for (Point p : hexominoNumberSet.getPoints()) {
+            Point pointToCheck = new Point(anchorLocation.x + p.x,
+                    anchorLocation.y + p.y);
+            if (!(this.isInBounds(pointToCheck.x, pointToCheck.y))) {
+                return false;
+            }
+            if (!tilesViaPoints.get(pointToCheck).canAddHex(hex)) {
+                canAdd = false;
+            }
+        }
 
-	/**
-	 * Retrieves the position where a hexomino is placed.
-	 * 
-	 * @param hex
-	 *            Hexomino to be found.
-	 * @return The Point p where the anchor of tile is located.
-	 */
-	public Point getHexominoLocation(Hexomino hex) {
-		return hexominoLocations.get(hex);
-	}
+        return canAdd;
+    }
 
-	/**
-	 * Adds the given Hexomino to the board. Must be a valid move.
-	 * 
-	 * @param anchorLocation
-	 *            Location of where the anchor of the hexomino will go.
-	 * @param hex
-	 *            The Hexomino to be added
-	 * 
-	 */
-	public void addHexomino(Point anchorLocation, Hexomino hex) {
-		HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
+    /**
+     * Retrieves the position where a hexomino is placed.
+     * 
+     * @param hex
+     *            Hexomino to be found.
+     * @return The Point p where the anchor of tile is located.
+     */
+    public Point getHexominoLocation(Hexomino hex) {
+        return hexominoLocations.get(hex);
+    }
 
-		hexominoLocations.put(hex, anchorLocation.getLocation());
+    /**
+     * Adds the given Hexomino to the board. Must be a valid move.
+     * 
+     * @param anchorLocation
+     *            Location of where the anchor of the hexomino will go.
+     * @param hex
+     *            The Hexomino to be added
+     * 
+     */
+    public void addHexomino(Point anchorLocation, Hexomino hex) {
+        HexominoNumberSet hexominoNumberSet = hex.getHexominoNumberSet();
 
-		for (Point p : hexominoNumberSet.getPoints()) {
-			Point pointToAdd = new Point(anchorLocation.x + p.x, anchorLocation.y + p.y);
-			if (tilesViaPoints.get(pointToAdd).canAddHex(hex) & isInBounds(pointToAdd.x, pointToAdd.y)) {
-				tilesViaPoints.get(pointToAdd).addHexomino(hex);
-			}
-		}
+        hexominoLocations.put(hex, anchorLocation.getLocation());
 
-		update();
-	}
+        for (Point p : hexominoNumberSet.getPoints()) {
+            Point pointToAdd = new Point(anchorLocation.x + p.x,
+                    anchorLocation.y + p.y);
+            if (tilesViaPoints.get(pointToAdd).canAddHex(hex)
+                    & isInBounds(pointToAdd.x, pointToAdd.y)) {
+                tilesViaPoints.get(pointToAdd).addHexomino(hex);
+            }
+        }
 
-	/**
-	 * Completely removes the specified Hexomino from the board.
-	 * 
-	 * Note that this removes the Hexomino only if it is the exact same
-	 * instance. If the Hexomino given is .equals() but not ==, it doesn't
-	 * matter, it won't be removed.
-	 */
-	public void removeHexomino(Hexomino hex) {
-		for (Tile t : getTiles()) {
-			if (t.getHexomino().isPresent()) {
-				if (t.getHexomino().get().getID().equals(hex.getID())) { // TODO
-					// is
-					// this
-					// correct
-					t.removeHex();
-				}
-			}
-		}
+        update();
+    }
 
-		hexominoLocations.remove(hex);
-		update();
-	}
+    /**
+     * Completely removes the specified Hexomino from the board.
+     * 
+     * Note that this removes the Hexomino only if it is the exact same
+     * instance. If the Hexomino given is .equals() but not ==, it doesn't
+     * matter, it won't be removed.
+     */
+    public void removeHexomino(Hexomino hex) {
+        for (Tile t : getTiles()) {
+            if (t.getHexomino().isPresent()) {
+                if (t.getHexomino().get().getID().equals(hex.getID())) { // TODO
+                    // is
+                    // this
+                    // correct
+                    t.removeHex();
+                }
+            }
+        }
 
-	/**
-	 * Gets the Tile at the specified position
-	 * 
-	 * @param row
-	 * @param column
-	 * @return The Tile found.
-	 */
-	public Tile getTile(int row, int column) {
-		return getTile(new Point(row, column));
-	}
+        hexominoLocations.remove(hex);
+        update();
+    }
 
-	/**
-	 * Gets the Tile at the specified position
-	 * 
-	 * @param p
-	 *            Point to get tile at.
-	 * @return The Tile found.
-	 */
-	public Tile getTile(Point p) {
-		return tilesViaPoints.get(p);
-	}
+    /**
+     * Gets the Tile at the specified position
+     * 
+     * @param row
+     * @param column
+     * @return The Tile found.
+     */
+    public Tile getTile(int row, int column) {
+        return getTile(new Point(row, column));
+    }
 
-	/**
-	 * Returns whether given row and column are in bounds in the existing game
-	 * region. I.E. if the given row and column are outside of the 12 by 12
-	 * grid, this will return false.
-	 * 
-	 * Note this has nothing to do with whether a tile is playable or not.
-	 * 
-	 * @param row
-	 * @param column
-	 * @return True if the location is in bounds
-	 */
-	private boolean isInBounds(int row, int column) {
-		if (row > HEIGHT_BY_ZERO || column > WIDTH_BY_ZERO) {
-			return false;
-		} else if (row < 0 || column < 0) {
-			return false;
-		} else
-			return true;
-	}
+    /**
+     * Gets the Tile at the specified position
+     * 
+     * @param p
+     *            Point to get tile at.
+     * @return The Tile found.
+     */
+    public Tile getTile(Point p) {
+        return tilesViaPoints.get(p);
+    }
 
-	/**
-	 * Gets a iterable of all the tiles.
-	 * 
-	 * You can use this any time you want to do some kind of operation on all of
-	 * the tiles. Please be careful and don't actually edit the tiles with a
-	 * controller or something unless it's really needed.
-	 * 
-	 * Note that these tiles are not in order if you use this method.
-	 * 
-	 * @return An iterable of all of the tiles.
-	 */
-	public Iterable<Tile> getTiles() {
-		LinkedList<Tile> tempList = new LinkedList<Tile>();
-		tilesViaPoints.forEach((k, v) -> tempList.add(v));
-		return tempList;
-	}
+    /**
+     * Returns whether given row and column are in bounds in the existing game
+     * region. I.E. if the given row and column are outside of the 12 by 12
+     * grid, this will return false.
+     * 
+     * Note this has nothing to do with whether a tile is playable or not.
+     * 
+     * @param row
+     * @param column
+     * @return True if the location is in bounds
+     */
+    private boolean isInBounds(int row, int column) {
+        if (row > HEIGHT_BY_ZERO || column > WIDTH_BY_ZERO) {
+            return false;
+        } else if (row < 0 || column < 0) {
+            return false;
+        } else
+            return true;
+    }
 
-	/**
-	 * Determines if this board has the hexomino hex played on it
-	 * 
-	 * @param hex
-	 *            the hex to be looked for
-	 * @return True if hex is found, otherwise false
-	 */
-	public boolean hasHex(Hexomino hex) {
-		return this.containsHexID(hex.getID());
-	}
+    /**
+     * Gets a iterable of all the tiles.
+     * 
+     * You can use this any time you want to do some kind of operation on all of
+     * the tiles. Please be careful and don't actually edit the tiles with a
+     * controller or something unless it's really needed.
+     * 
+     * Note that these tiles are not in order if you use this method.
+     * 
+     * @return An iterable of all of the tiles.
+     */
+    public Iterable<Tile> getTiles() {
+        LinkedList<Tile> tempList = new LinkedList<Tile>();
+        tilesViaPoints.forEach((k, v) -> tempList.add(v));
+        return tempList;
+    }
 
-	public boolean containsHexID(UUID id) {
+    /**
+     * Determines if this board has the hexomino hex played on it
+     * 
+     * @param hex
+     *            the hex to be looked for
+     * @return True if hex is found, otherwise false
+     */
+    public boolean hasHex(Hexomino hex) {
+        return this.containsHexID(hex.getID());
+    }
 
-		for (Tile t : this.getTiles()) {
-			if (t.hasHex()) {
-				if (t.getHexomino().get().getID().equals(id)) {
-					return true;
-				}
-			}
-		}
+    public boolean containsHexID(UUID id) {
 
-		return false;
-	}
+        for (Tile t : this.getTiles()) {
+            if (t.hasHex()) {
+                if (t.getHexomino().get().getID().equals(id)) {
+                    return true;
+                }
+            }
+        }
 
-	public void setUnplayableRectangle(Point topLeft, Point bottomRight) {
+        return false;
+    }
 
-		for (int i = topLeft.x; i < bottomRight.x; i++) {
+    public void setUnplayableRectangle(Point topLeft, Point bottomRight) {
 
-			for (int j = topLeft.y; j < bottomRight.y; j++) {
-				this.getTile(i, j).setPlayable(false);
-			}
+        for (int i = topLeft.x; i < bottomRight.x; i++) {
 
-		}
-	}
-	
-	/**
-	 * Adds a hint to the list of hints.
-	 * @param hint
-	 */
-	public void addHint(Hint hint) {
-		this.hints.add(hint);
-		update();
-	}
-	
-	public void removeHint(Hint hint) {
-		this.hints.remove(hint);
-		update();
-	}
-	
-	public List<Hint> getHints() {
-		return this.hints;
-	}
-	
-	public List<Point> getPoints(Point anchor, HexominoNumberSet numSet) {
-		List<Point> returnPoints = new LinkedList<Point> ();
-		
-		for(Point p : numSet.getPoints()) {
-			returnPoints.add(new Point(anchor.x + p.x, anchor.y + p.y));
-		}
-		
-		return returnPoints;
-	}
-	
-	@Override
-	public void registerObserver(Observer observer) {
-		this.observers.add(observer);
-	}
+            for (int j = topLeft.y; j < bottomRight.y; j++) {
+                this.getTile(i, j).setPlayable(false);
+            }
 
-	@Override
-	public void update() {
-		for (Observer o : observers) {
-			o.updated();
-		}
-	}
-	
+        }
+    }
+
+    /**
+     * Adds a hint to the list of hints.
+     * 
+     * @param hint
+     */
+    public void addHint(Hint hint) {
+        this.hints.add(hint);
+        update();
+    }
+
+    public void removeHint(Hint hint) {
+        this.hints.remove(hint);
+        update();
+    }
+
+    public List<Hint> getHints() {
+        return this.hints;
+    }
+
+    public List<Point> getPoints(Point anchor, HexominoNumberSet numSet) {
+        List<Point> returnPoints = new LinkedList<Point>();
+
+        for (Point p : numSet.getPoints()) {
+            returnPoints.add(new Point(anchor.x + p.x, anchor.y + p.y));
+        }
+
+        return returnPoints;
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        this.observers.add(observer);
+        update();
+    }
+
+    @Override
+    public void update() {
+        for (Observer o : observers) {
+            o.updated();
+        }
+    }
 
 }
--- KabasujiSinon/src/sinon/views/BoardView.java
+++ KabasujiSinon/src/sinon/views/BoardView.java
@@ -21,91 +21,96 @@ import sinon.models.Hint;
  * @author Brian
  *
  */
-public class BoardView extends JPanel implements Observer{
+public class BoardView extends JPanel implements Observer {
 
-	private static final long serialVersionUID = -5335188988716751398L;
-	JPanel boardPanel;
-	private Board board;
-	Collection<TileView> tileViews;
+    private static final long serialVersionUID = -5335188988716751398L;
+    JPanel boardPanel;
+    private Board board;
+    Collection<TileView> tileViews;
 
-	public BoardView(Board board) {
-		this.board = Objects.requireNonNull(board);
-		this.board.registerObserver(this);
-		this.boardPanel = new JPanel();
-		this.boardPanel.setLayout(new GridLayout(12, 12));
-		this.boardPanel.setBounds(80, 26, 300, 300);
-		this.boardPanel.setBackground(Color.BLACK);
-		this.boardPanel.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-		this.tileViews = new ArrayList<TileView>();
+    public BoardView(Board board) {
+        this.board = Objects.requireNonNull(board);
 
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
-				initializeTile(i, j);
-			}
-		}
-	}
+        this.boardPanel = new JPanel();
+        this.boardPanel.setLayout(new GridLayout(12, 12));
+        this.boardPanel.setBounds(80, 26, 300, 300);
+        this.boardPanel.setBackground(Color.BLACK);
+        this.boardPanel
+                .setBorder(BorderFactory.createLineBorder(Color.black, 1));
+        this.tileViews = new ArrayList<TileView>();
 
-	/**
-	 * Initialize a TileView
-	 * 
-	 * @param row
-	 *            The row the tile is on
-	 * @param column
-	 *            The column the tile is on
-	 */
-	private void initializeTile(int row, int column) {
-		TileView tileView = new TileView(board.getTile(row, column));
-		if (tileView.getTile().isPlayable()) {
-			tileView.setBackground(Color.white);
-		} else {
-			if (tileView.getTile().isPlayable() == false) {
-				tileView.setBackground(Color.black);
-			}
-		}
-		tileView.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
-		this.tileViews.add(tileView);
-		boardPanel.add(tileView);
-	}
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                initializeTile(i, j);
+            }
+        }
+        this.board.registerObserver(this);
+    }
 
-	/**
-	 * Gives the collection of all TileViews that make up the BoardView
-	 * 
-	 * @return The TileViews as a Collection
-	 */
-	public Collection<TileView> getTileViews() {
-		return tileViews;
-	}
+    /**
+     * Initialize a TileView
+     * 
+     * @param row
+     *            The row the tile is on
+     * @param column
+     *            The column the tile is on
+     */
+    private void initializeTile(int row, int column) {
+        TileView tileView = new TileView(board.getTile(row, column));
+        if (tileView.getTile().isPlayable()) {
+            tileView.setBackground(Color.white);
+        } else {
+            if (tileView.getTile().isPlayable() == false) {
+                tileView.setBackground(Color.black);
+            }
+        }
+        tileView.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
+        this.tileViews.add(tileView);
+        boardPanel.add(tileView);
+    }
 
-	public void setShadow(List<Point> pointsToShadow) {
-		for (TileView tv : this.tileViews) {
-			if(tv.setShadow(pointsToShadow.contains(new Point(tv.row, tv.column))))
-				tv.updated();
-		}
-	}
-	
-	public void updateHints() {
-		List<Hint> hints = this.board.getHints(); 
-		List<Point> absoluteHintLocations = new LinkedList<Point> ();
-		
-		for(Hint h : hints) {
-			absoluteHintLocations.addAll(this.board.getPoints(h.getAnchor(), h.getHexominoNumberSet()));
-		}
-		
-		for(TileView tv : this.tileViews) {
-			if(absoluteHintLocations.contains(new Point(tv.getRow(), tv.getColumn()))) {
-				tv.setHint(true);
-			} else {
-				tv.setHint(false);
-			}
-		}
-		
-	}
+    /**
+     * Gives the collection of all TileViews that make up the BoardView
+     * 
+     * @return The TileViews as a Collection
+     */
+    public Collection<TileView> getTileViews() {
+        return tileViews;
+    }
 
-	@Override
-	public void updated() {
-		updateHints();
-		for(TileView tv : tileViews) {
-			tv.updated();
-		}
-	}
+    public void setShadow(List<Point> pointsToShadow) {
+        for (TileView tv : this.tileViews) {
+            if (tv.setShadow(
+                    pointsToShadow.contains(new Point(tv.row, tv.column))))
+                tv.updated();
+        }
+    }
+
+    public void updateHints() {
+        List<Hint> hints = this.board.getHints();
+        List<Point> absoluteHintLocations = new LinkedList<Point>();
+
+        for (Hint h : hints) {
+            absoluteHintLocations.addAll(this.board.getPoints(h.getAnchor(),
+                    h.getHexominoNumberSet()));
+        }
+
+        for (TileView tv : this.tileViews) {
+            if (absoluteHintLocations
+                    .contains(new Point(tv.getRow(), tv.getColumn()))) {
+                tv.setHint(true);
+            } else {
+                tv.setHint(false);
+            }
+        }
+
+    }
+
+    @Override
+    public void updated() {
+        updateHints();
+        for (TileView tv : tileViews) {
+            tv.updated();
+        }
+    }
 }
