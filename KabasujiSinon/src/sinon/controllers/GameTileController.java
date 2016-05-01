c581b523ed20486793101e77676eae66ff860721
Brian McCarthy <brianmcca1@gmail.com>
2016-04-30 23:27:43 -0400

    Adjusted so you can't move Hexs in a Release Level in the Game

--- KabasujiSinon/src/sinon/controllers/GameTileController.java
+++ KabasujiSinon/src/sinon/controllers/GameTileController.java
@@ -2,12 +2,16 @@ package sinon.controllers;
 
 import sinon.models.Board;
 import sinon.models.Level;
+import sinon.models.data.LevelType.Types;
+import sinon.moves.MoveInBoard;
+import sinon.moves.MoveToBoardFromBullpen;
 import sinon.views.MainView;
 import sinon.views.TileView;
 
 /**
- * This is the tile controller in the game that is mostly responsible 
- * for firing off moves, setting shadows, etc.
+ * This is the tile controller in the game that is mostly responsible for firing
+ * off moves, setting shadows, etc.
+ * 
  * @author kartik
  *
  */
@@ -22,12 +26,51 @@ public class GameTileController extends TileController {
 		return level.getBoard();
 	}
 
-
 	@Override
 	public void handleRightClick() {
 		// Do nothing.
 	}
 
-	
+	@Override
+	public void handleLeftClick() {
+
+		if (level.hasSelected()) {
+
+			if (level.getBullpen().containsHexID(level.getSelectedHexomino().get().getID())) {
+				// else move from bullpen to board
+				MoveToBoardFromBullpen move = new MoveToBoardFromBullpen(this.level, this.tileView.getRow(),
+						this.tileView.getColumn());
+
+				if (move.doMove()) {
+					level.pushMove(move);
+					System.out.println("Move successfully completed!");
+					level.deselect();
+				} else {
+					System.out.println("There was some error doing the move.");
+				}
+			}
+
+			else if (level.getBoard().containsHexID(level.getSelectedHexomino().get().getID())) {
+				// then move from board to board
+				int x, y;
+				x = level.getBoard().getHexominoLocation(level.getSelectedHexomino().get()).x;
+				y = level.getBoard().getHexominoLocation(level.getSelectedHexomino().get()).y;
+				MoveInBoard move = new MoveInBoard(level, x, y, this.tileView.getRow(), this.tileView.getColumn());
+
+				if (move.doMove()) {
+					level.pushMove(move);
+					System.out.println("The move was successfully completed!");
+					level.deselect();
+				} else {
+					System.out.println("The move couldn't be completed!");
+				}
+
+			}
+		} else {
+			if (this.tileView.getTile().hasHex() & (this.level.getLevelData().getLevelType() != Types.RELEASE)) {
+				level.select(this.tileView.getTile().getHexomino().get());
+			}
+		}
+	}
 
 }
