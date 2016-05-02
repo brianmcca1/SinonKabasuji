98f995d489ec99f473925e87e101145d872895f5
SkiingxMoose <jodesmond@live.com>
2016-05-02 19:10:45 -0400

    Fixed up a broken test case after a change was made to tile.

--- KabasujiSinon/Tests/sinon/models/TestTile.java
+++ KabasujiSinon/Tests/sinon/models/TestTile.java
@@ -11,97 +11,94 @@ import org.junit.Test;
 
 public class TestTile {
 
-	@Test
-	public void testAddNormal() {
-		Point p = new Point(0, 0);
-		Tile t1 = new Tile(p, true);
-		Hexomino hex = Hexomino.getExampleHexomino();
-		Hexomino hex2 = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
-		assertEquals(t1.canAddHex(hex2), true);
+    @Test
+    public void testAddNormal() {
+        Point p = new Point(0, 0);
+        Tile t1 = new Tile(p, true);
+        Hexomino hex = Hexomino.getExampleHexomino();
+        Hexomino hex2 = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
+        assertEquals(t1.canAddHex(hex2), true);
 
-		t1.addHexomino(hex2);
+        t1.addHexomino(hex2);
 
-		assertEquals(t1.getHexomino(), Optional.of(hex2));
+        assertEquals(t1.getHexomino(), Optional.of(hex2));
 
-		assertEquals(t1.getLocation(), new Point(0, 0));
+        assertEquals(t1.getLocation(), new Point(0, 0));
 
-		assertEquals(t1.canAddHex(hex), false);
+        assertEquals(t1.canAddHex(hex), false);
 
-		assertEquals(t1.hasHex(), true);
+        assertEquals(t1.hasHex(), true);
 
-		assertTrue(t1.removeHex());
+        assertTrue(t1.removeHex());
 
-		assertEquals(t1.hasHex(), false);
+        assertEquals(t1.hasHex(), false);
 
-	}
+    }
 
-	@Test
-	public void testGetEmpty() {
-		Point p = new Point(0, 0);
-		Tile t1 = new Tile(p, true);
+    @Test
+    public void testGetEmpty() {
+        Point p = new Point(0, 0);
+        Tile t1 = new Tile(p, true);
 
-		Optional<Hexomino> hex = Optional.empty();
+        Optional<Hexomino> hex = Optional.empty();
 
-		hex = t1.getHexomino();
+        hex = t1.getHexomino();
 
-		assertEquals(hex.isPresent(), false);
-	}
+        assertEquals(hex.isPresent(), false);
+    }
 
-	@Test
-	public void testRemoveHex() {
-		Point p = new Point(0, 0);
-		Tile t1 = new Tile(p, true);
+    @Test
+    public void testRemoveHex() {
+        Point p = new Point(0, 0);
+        Tile t1 = new Tile(p, true);
 
-		Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
+        Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
 
-		t1.addHexomino(hex);
+        t1.addHexomino(hex);
 
-		assertTrue(t1.removeHex());
+        assertTrue(t1.removeHex());
 
-		Optional<Hexomino> emptyHex = Optional.empty();
-		emptyHex = t1.getHexomino();
-		assertEquals(emptyHex.isPresent(), false);
-	}
+        Optional<Hexomino> emptyHex = Optional.empty();
+        emptyHex = t1.getHexomino();
+        assertEquals(emptyHex.isPresent(), false);
+    }
 
-	@Test
-	public void testRemoveEmpty() {
-		Point p = new Point(0, 0);
-		Tile t1 = new Tile(p, true);
+    @Test
+    public void testRemoveEmpty() {
+        Point p = new Point(0, 0);
+        Tile t1 = new Tile(p, true);
 
-		assertFalse(t1.removeHex());
-	}
+        assertFalse(t1.removeHex());
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void testAddFull() {
-		Tile t1 = new Tile(new Point(0, 0), true);
+    @Test
+    public void testAddFull() {
+        Tile t1 = new Tile(new Point(0, 0), true);
+        Hexomino hex = Hexomino.getExampleHexomino();
+        Hexomino hex2 = Hexomino.getExampleHexomino();
 
-		Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
+        assertTrue(t1.addHexomino(hex) == true);
+        assertTrue(t1.addHexomino(hex2) == false);
+    }
 
-		t1.addHexomino(hex);
+    public void addUnplayable() {
+        Tile t1 = new Tile(new Point(0, 0), false);
+        Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
+        assertEquals(t1.canAddHex(hex), false);
 
-		t1.addHexomino(hex);
-	}
+        assertTrue(t1.addHexomino(hex) == false);
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void addUnplayable() {
-		Tile t1 = new Tile(new Point(0, 0), false);
-		Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
-		assertEquals(t1.canAddHex(hex), false);
+    @Test
+    public void setPlayable() {
+        Tile t1 = new Tile(new Point(0, 0), false);
+        Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
+        assertFalse(t1.canAddHex(hex));
 
-		t1.addHexomino(hex);
+        t1.setPlayable(true);
 
-	}
-
-	@Test
-	public void setPlayable() {
-		Tile t1 = new Tile(new Point(0, 0), false);
-		Hexomino hex = new Hexomino(0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1);
-		assertFalse(t1.canAddHex(hex));
-
-		t1.setPlayable(true);
-
-		t1.addHexomino(hex);
-		assertEquals(t1.getHexomino().get(), hex);
-	}
+        t1.addHexomino(hex);
+        assertEquals(t1.getHexomino().get(), hex);
+    }
 
 }
