117f8d630254cc7da0a04b163f1d44b88ddeab66
SkiingxMoose <jodesmond@live.com>
2016-05-02 19:47:13 -0400

    Added testing in the model for duplicate rotation in the bullpen.

--- KabasujiSinon/Tests/sinon/moves/TestMoveRotateCC.java
+++ KabasujiSinon/Tests/sinon/moves/TestMoveRotateCC.java
@@ -6,23 +6,43 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import sinon.models.Hexomino;
+import sinon.models.LightningLevel;
 import sinon.models.NumberSetFactory;
 
 public class TestMoveRotateCC {
 
-	@Test
-	public void test() {
-		Hexomino hex = new Hexomino(NumberSetFactory.getByNumbers(0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0));
-		RotateHexominoClockwiseMove fhm = new RotateHexominoClockwiseMove(hex);
-		assertTrue(fhm.doMove());
-		assertTrue(fhm.undo());
-	}
+    @Test
+    public void test() {
+        Hexomino hex = new Hexomino(NumberSetFactory.getByNumbers(0, 0, 1, 0, 2,
+                0, 3, 0, 4, 0, 5, 0));
+        RotateHexominoClockwiseMove fhm = new RotateHexominoClockwiseMove(hex);
+        assertTrue(fhm.doMove());
+        assertTrue(fhm.undo());
+    }
 
-	@Test(expected = NullPointerException.class)
-	public void testNull() {
-		Hexomino hex = null;
-		RotateHexominoClockwiseMove fhm = new RotateHexominoClockwiseMove(hex);
-		assertFalse(fhm.doMove());
-		assertFalse(fhm.undo());
-	}
+    @Test(expected = NullPointerException.class)
+    public void testNull() {
+        Hexomino hex = null;
+        RotateHexominoClockwiseMove fhm = new RotateHexominoClockwiseMove(hex);
+        assertFalse(fhm.doMove());
+        assertFalse(fhm.undo());
+    }
+
+    @Test
+    public void testDuplicatesInBullpen() {
+        LightningLevel level = LightningLevel.getExampleLevel();
+        level.getBullpen().addHexomino(Hexomino.getExampleHexomino());
+        RotateHexominoClockwiseMove m = new RotateHexominoClockwiseMove(
+                level.getBullpen().getPieces().get(0));
+        assertTrue(m.doMove());
+
+        // Now the 0th hexomino has been rotated clockwise, we must verify that
+        // the second hasn't rotated.
+        Hexomino h0 = level.getBullpen().getPieces().get(0);
+        Hexomino h1 = level.getBullpen().getPieces().get(1);
+
+        assertFalse("Numbersets should be different",
+                h0.getNormalizedPoints().equals(h1.getNormalizedPoints()));
+    }
+
 }
