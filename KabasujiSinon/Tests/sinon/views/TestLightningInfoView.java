29b2fb48695f392e19d7d968e6e38074ff9f21b3
SkiingxMoose <jodesmond@live.com>
2016-05-02 19:05:25 -0400

    Continued work on the test, basically done.

--- KabasujiSinon/Tests/sinon/views/TestLightningInfoView.java
+++ KabasujiSinon/Tests/sinon/views/TestLightningInfoView.java
@@ -1,6 +1,7 @@
 package sinon.views;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,15 +10,40 @@ import sinon.models.LightningLevel;
 
 public class TestLightningInfoView {
 
+    /** Pretty basic LightningLevel setup in the {@link #setUp()} method. */
+    LightningLevel level;
+
     @Before
     public void setUp() throws Exception {
-
-        LightningLevel level = LightningLevel.getExampleLevel();
+        level = LightningLevel.getExampleLevel();
     }
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testCreateLightningInfoView() {
+        LightningInfoView v = new LightningInfoView(false, level);
+        assertNotNull(v.infoLabel);
+        assertTrue(v.infoLabel.isVisible());
+        assertTrue(v.level.equals(level));
+        assertTrue(v.getComponents().length > 0);
+    }
+
+    @Test
+    public void testEditable() {
+        LightningInfoView vNonEdit = new LightningInfoView(false, level);
+        assertTrue(vNonEdit.timeLeftField.isEditable() == false);
+        LightningInfoView vEdit = new LightningInfoView(true, level);
+        assertTrue(vEdit.timeLeftField.isEditable() == true);
+    }
+
+    @Test
+    public void testRegistersObservable() {
+        // Test that lightning is being observed.
+        LightningInfoView v = new LightningInfoView(false, level);
+        level.update();
+        assertTrue(v.timeLeftField.getText().contains("30"));
+        level.tickTimeLeft();
+        level.tickTimeLeft();
+        assertTrue(v.timeLeftField.getText().contains("28"));
     }
 
 }
